eefd452d7683941c2333386c5d9199cf170c9040
Quintin Henn <quintin.henn@gmail.com>
2021-12-02 10:45:28 +0100

    refactor: Added java doc comment

--- src/main/java/za/web/skerwe/adventofcode2021/util/InputFileReader.java
+++ src/main/java/za/web/skerwe/adventofcode2021/util/InputFileReader.java
@@ -9,6 +9,14 @@ import java.util.List;
 
 public class InputFileReader {
 
+  /**
+   * Read a text file into an array of strings.
+   * The input files are under <code>src/main/resources</code>
+   *
+   * @param fileName the file name to read
+   * @return an array of strings representing each line in the file
+   * @throws IOException when the file cannot be found
+   */
   public static String[] readInputFile(String fileName) throws IOException {
     List<String> lines = new ArrayList<>();
 
