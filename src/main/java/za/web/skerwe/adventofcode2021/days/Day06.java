27103493b66c76cd94ee77f68863de639b872380
Quintin Henn <quintin.henn@gmail.com>
2021-12-07 08:51:24 +0100

    fix: update description of part one

--- src/main/java/za/web/skerwe/adventofcode2021/days/Day06.java
+++ src/main/java/za/web/skerwe/adventofcode2021/days/Day06.java
@@ -13,7 +13,7 @@ public class Day06 extends AdventDay {
   public Day06() {
     id = 6;
     name = "Day 6: Lanternfish";
-    partOneDescription = "lanternfish would there be after 80 days?";
+    partOneDescription = "How many lanternfish would there be after 80 days?";
     partTwoDescription = "How many lanternfish would there be after 256 days?";
 
     inputPart1 = "src/main/resources/input06-p1.txt";
